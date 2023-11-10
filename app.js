b0c75fa019997531e08b5073acfae137364d5ad9
Erika Vestin <erbj1201@student.miun.se>
2023-11-10 13:41:46 +0100

    Post, get ok, sparas i json

--- app.js
+++ app.js
@@ -4,35 +4,43 @@ const Joi = require('joi');
 const express = require('express');
 //Importera fs för att kommunicera med json-fil
 const fs = require('fs').promises;
-const COURSES_FILE_PATH = 'courses.json';
+const jsonCourses = 'courses.json'
 //Skapa app med express och konfigurera att analysera förfrågningar med JSON
 const app = express ();
 app.use(express.json());
 
-const courses = [
-    {id: 1, courseId: 'DT111G', courseName: 'course1', coursePeriod: '1'},
-    {id: 2, courseId: 'DT222G', courseName: 'course2', coursePeriod: '2'},
-    {id: 3, courseId: 'DT333G', courseName: 'course3', coursePeriod: '3'},
-];
 //Validera med hjälp av Joi
 const courseSchema = Joi.object({
     courseId: Joi.string().min(4).required(),
     courseName: Joi.string().min(4).required(),
-    coursePeriod: Joi.string().max(2).required(),
+    coursePeriod: Joi.number().max(4).required(),
 });
 //Routes
 app.get('/', async (req, res) => {
-res.send('Testar app.js')
+try {
+    const data = await fs.readFile(jsonCourses);
+    const courses = JSON.parse(data);
+    res.send(courses);
+} catch (error) {
+    console.error(error);
+    res.status(500).send('Tekniskt fel')
+}
 });
 //Hämta alla kurser
-app.get('/courses', (req, res) =>{
-    //Hämta data från API
-res.send(courses);
+app.get('/courses', async (req, res) =>{
+    try {
+        const data = await fs.readFile(jsonCourses);
+        const courses = JSON.parse(data);
+        res.send(courses);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Tekniskt fel')
+    }
 });
 //Hämta specifik kurs med id
 app.get('/courses/:id', (req, res)=>{
     //Hitta kurs med angivet id och omvandla id från string till integer
-    const course = courses.find(c => c.id===parseInt(req.params.id));
+    const course = course.find(c => c.id===parseInt(req.params.id));
     //Kontrollera om kurs finns, om inte returnera 404 
     if (!course) res.status(404).send ('Kursen kunde inte hittas.');
     //Returnera kurs med angivet id
@@ -40,39 +48,57 @@ app.get('/courses/:id', (req, res)=>{
 });
 
 //Lägg till kurs
-app.post('/courses', (req, res) => {
-    //Anger vad bodyn behöver innehålla
-    const { courseId, courseName, coursePeriod } = req.body;
-    // Validera input med Joi
-    const { error } = courseSchema.validate({ courseId, courseName, coursePeriod }, { abortEarly: false });
-    if (error){
-        const errorMessages = error.details.map(err => err.message);
-        return res.status(400).send(errorMessages);
-    } 
-    // Create a new course object
-    const newCourse = {
-        id: courses.length + 1, // You might want to generate a unique ID here
-        courseId,
-        courseName,
-        coursePeriod
-    };
-    // Add the new course to the courses array
-    courses.push(newCourse);
-    // Return the newly created course in the response
-    res.send(`Kurs tillagd: ${JSON.stringify(newCourse)}`);
+app.post('/courses', async (req, res) => {
+    try {
+        const { courseId, courseName, coursePeriod } = req.body;
+        // Validate the request body using the defined schema
+        const { error } = courseSchema.validate({ courseId, courseName, coursePeriod }, { abortEarly: false });
+        if (error) {
+            const errorMessages = error.details.map(err => err.message);
+            return res.status(400).send(errorMessages);
+        }
+        // Read existing courses from the JSON file
+        const data = await fs.readFile(jsonCourses);
+        const courses = JSON.parse(data);
+        // Create a new course object
+        const newCourse = {
+            _id: courses.length + 1,
+            courseId,
+            courseName,
+            coursePeriod
+        };
+        // Add the new course to the existing courses array
+        courses.push(newCourse);
+        // Write the updated courses array back to the JSON file
+        await fs.writeFile(jsonCourses, JSON.stringify(courses, null, 2));
+        res.send(`Kurs tillagd: ${JSON.stringify(newCourse)}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Kunde inte lägga till kurs');
+    }
 });
 
 //Radera kurs med specifikt id
-app.delete('/courses/:id', (req, res) => {
-  //Hitta kurs med angivet id och omvandla id från string till integer
-  const course = courses.find(c => c.id===parseInt(req.params.id));
-  //Kontrollera om kurs finns, om inte returnera 404 
-  if(!course) return res.status(404).send ('Kursen kunde inte hittas.');
-  //radera kurs
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-//Returnera 
-res.send('Kurs raderad');
+app.delete('/courses/:id', async (req, res) => {
+    try {
+        const courseIdToDelete = parseInt(req.params.id);
+        // Read existing courses from the JSON file
+        const data = await fs.readFile(jsonCourses);
+        const courses = JSON.parse(data);
+        // Find the index of the course with the specified ID
+        const index = courses.findIndex(course => course.id === courseIdToDelete);
+        if (index === -1) {
+            return res.status(404).send('Kursen kunde inte hittas.');
+        }
+        // Remove the course from the array
+        courses.splice(index, 1);
+        // Write the updated courses array back to the JSON file
+        await fs.writeFile(jsonCourses, JSON.stringify(courses, null, 2));
+        res.send('Kurs raderad.');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Kunde inte radera kurs');
+    }
 });
 
 //Port
